48bbec05f19f216ef1729666fe2fefaf368ddf3a
Abishek1221 <abisheks913@gmail.com>
2024-06-30 15:50:21 +0545

    ..

--- frontend/src/components/Shop/Layout/DashboardHeader.jsx
+++ frontend/src/components/Shop/Layout/DashboardHeader.jsx
@@ -12,7 +12,7 @@ const DashboardHeader = () => {
     <div className="font-Roboto">
       <div className="bg-primary-black text-white h-[80px] flex items-center justify-between px-4">
         <div className="ml-11 mb-2 flex items-center">
-          <Link to="/dashboard">
+          <Link to="/">
             <h3 className="text-2xl font-bold">
               Ramro<span className="text-[#76ABAE]">Sale</span>
             </h3>
